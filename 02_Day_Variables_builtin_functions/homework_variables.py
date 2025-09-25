4ecdec37b511ff5b8b0259b2c10a0f63c3f44ece
chinna <bansuay88@gmail.com>
2025-09-25 14:54:15 +0700

    Create homework_variables.py

---
+++ 02_Day_Variables_builtin_functions/homework_variables.py
@@ -0,0 +1,13 @@
+# Day 2: 30 days of python programming
+first_name = "chinnaphong"
+last_name = "roikaew"
+full_name = first_name + " " + last_name
+country = "thailand"
+city = "bangkok"
+age = 16
+year = "highschool 11"
+is_married = 'true'
+is_true = 'true'
+is_light_on = 'true'
+is_married, is_true, is_light_on = 'false'
+
