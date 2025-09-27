1de38e89a87417e6bd193658f2afd0da8010839d
chinna <bansuay88@gmail.com>
2025-09-27 11:23:56 +0700

    Create homework_operators.py

---
+++ 03_Day_Operators/homework_operators.py
@@ -0,0 +1,48 @@
+radius = input('enter radius ')
+radius = float(radius)
+area_of_circle = 3.14*(radius ** 2)
+print(area_of_circle)
+
+
+age = 16
+height = 172.5
+complex = 25 - 100j
+
+b = float(input('enter base: '))
+h = float(input('enter height: '))
+area = 0.5 * b * h
+print("area of a circle is", area)
+
+side_a = float(input('enter side a of triangle: '))
+side_b = float(input('enter side b of triangle: '))
+side_c = float(input('enter side c of triangle: '))
+perimeter = side_a + side_b + side_c
+print('the perimeter of the triangle is', perimeter)
+
+length = float(input('enter length of triangle: '))
+width = float(input('enter width of your triangle: '))
+area = length * width
+perimeter = 2*(length + width)
+print('area of the rectangle is', area)
+print('perimeter of the reactangle', perimeter)
+
+radius = float(input('enter the radius of the circle: '))
+area = 3.14 * (radius * radius)
+circumference = 2 * (3.14 * radius)
+print('area of the circle is', area)
+print('circumference of the circle is', circumference)
+
+a = len('python')
+b = len('dragon')
+a != b
+
+for i in a, b:
+  'on' in i
+
+sentence = 'I hope this course is not full of jargon'
+'jargon' in sentence
+
+for i in a, b:
+  truth = 'on' not in i
+truth = 'true'
+print(truth)
