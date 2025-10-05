4908135951357c413ef5fa707904a74a5ee952b0
chinna <bansuay88@gmail.com>
2025-10-05 17:28:43 +0700

    Create homework_dictionaries.py

---
+++ 08_Day_Dictionaries/homework_dictionaries.py
@@ -0,0 +1,27 @@
+dog = {}
+print(f'type of variable \'dog\' is : {type(dog)}')
+dog['name'] = 'kylo'
+dog['colour'] = 'cream'
+dog['breed'] = 'shiba inu'
+dog['legs'] = 4
+dog['age'] = 9
+print(dog)
+dog_copy = ((dog.copy()).clear())
+print(dog_copy)
+
+
+student = dict()
+type = ['first name', 'last name', 'gender', 'age', 'martial status', 'skill', 'country', 'city', 'address']
+detail = ['chinnaphong', 'roikaew', 'man', '16', 'single', 'none', 'thailand', 'bangkok', 'bangmod']
+another = zip(type, detail)
+for a, b in another:
+  student[a] = b
+  print(student)
+
+หรือ
+
+student = dict()
+type = ['first name', 'last name', 'gender', 'age', 'martial status', 'skill', 'country', 'city', 'address']
+detail = ['chinnaphong', 'roikaew', 'man', '16', 'single', 'none', 'thailand', 'bangkok', 'bangmod']
+for i, a in enumerate(type):
+  student[a] = detail[i]
