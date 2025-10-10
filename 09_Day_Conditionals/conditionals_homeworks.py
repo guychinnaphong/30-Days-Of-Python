84399178c1309d80a7bf23fbb7f85451bfe002cb
chinna <bansuay88@gmail.com>
2025-10-10 16:58:59 +0700

    Create conditionals_homeworks.py

---
+++ 09_Day_Conditionals/conditionals_homeworks.py
@@ -0,0 +1,7 @@
+inputs = int(input('enter your age: '))
+print('you are old enough to drive') if inputs >18 else print('you need', (18  - inputs), 'more years to learn to drive.')
+
+my_age = 16
+your_age = input('please enter your age: ')
+diff_age = ads
+if your_age >16
