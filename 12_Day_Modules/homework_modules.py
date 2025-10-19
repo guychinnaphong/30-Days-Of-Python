fa3584a20ba11e199b24362a632ffa51ddf9efa2
chinna <bansuay88@gmail.com>
2025-10-19 12:58:47 +0700

    Update homework_modules.py

--- 12_Day_Modules/homework_modules.py
+++ 12_Day_Modules/homework_modules.py
@@ -12,3 +12,17 @@ def random_user_id():
   return numbers
 
 
+import string
+from random import random, randint
+character_list = list(string.ascii_letters + string.digits)
+def user_id_gen_by_user():
+  character = int(input('please enter amount of character you want in your user id : '))
+  amount = int(input('please enter amount of id you want to generate'))
+  for i in range(amount):
+    user_id = [character_list[i] for i in range(character)]
+  return user_id
+
+  
+  user_id = ''.join([character_list[randint(0, len(character_list) - 1)] for i in range(character)])
+  six_user_id = [user_id for _ in range(6)]
+  return six_user_id
