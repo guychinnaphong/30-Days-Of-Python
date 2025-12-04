ba0884e8849502d51bcacefb944ae81ccd3aac98
chinna <bansuay88@gmail.com>
2025-12-04 15:44:57 +0700

    making more built in function

--- 02_Day_Variables_builtin_functions/useful built-in function.py
+++ 02_Day_Variables_builtin_functions/useful built-in function.py
@@ -5,7 +5,8 @@ any(iterable) # similar functioning to or?
 ascii(object) # return stri
 breakpoint(*arg, **kws) # not sure what it's purpose yet
 callable(object)
-chr(i) # return unicode point of an integer?
+chr(i) # return unicode point of an integer? opposite of ord
+ord(c) # take unicode value and convert it into a character, opposite of chr
 delattr(object, name) # argument is object and string, get more information soon
 filter(function, iterable) # use to filter out some word using purpose of that function
 strin(x) # turn a value into word type string
@@ -20,3 +21,14 @@ list(iterable) # return value as a list of iterable, can not be used on a single
 map(function, iterable) # return an iterator that applies function to every item of iterable
 max(iterable, key="insert function here whether it's custom or built in")
 min(iterable, key="insert function here whether it's custom or built in")
+oct(x) # convert an integer into octal number string
+print(objects, sep=' ', end='\n', file=sys.stdout, flush=False)
+range(start, stop, step) # get function to only work in range we set often use in loop
+round(number, "insert decimal digits you want to round up") 
+set(iterable) # convert an iterable to set(set in mathematic)
+setattr(object, name, value) 
+slice(stop) or slice(start, stop, step) #slice a part of an argument out
+sorted(iterable, key=None, reverse=False) #sort out an iterable
+sum(iterable, start=0) #sum all the iterable up
+tuple(iterable) #convert an iterable into tuple
+type(object) or type(name,bases,dict,**kwds)# get type of that value or keyword and something
