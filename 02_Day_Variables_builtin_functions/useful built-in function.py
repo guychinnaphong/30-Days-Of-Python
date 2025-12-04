50f6a46005aa487449edf9ef96b5552b846e9f0f
chinna <bansuay88@gmail.com>
2025-12-04 13:41:01 +0700

    Update useful built-in function.py

--- 02_Day_Variables_builtin_functions/useful built-in function.py
+++ 02_Day_Variables_builtin_functions/useful built-in function.py
@@ -7,3 +7,16 @@ breakpoint(*arg, **kws) # not sure what it's purpose yet
 callable(object)
 chr(i) # return unicode point of an integer?
 delattr(object, name) # argument is object and string, get more information soon
+filter(function, iterable) # use to filter out some word using purpose of that function
+strin(x) # turn a value into word type string
+int(x) # turn a value into integer number
+float(x) # turn a value into decimal number
+hex(x) # connvert an integer to a hexdecimal string
+input(prompt) # get input of something
+isinstance(object, classinfo) # return True or False, can be use to check type of an object without using if else
+issubclass(class, classinfo) # return True or False
+len(x) # find length of an object
+list(iterable) # return value as a list of iterable, can not be used on a single variable
+map(function, iterable) # return an iterator that applies function to every item of iterable
+max(iterable, key="insert function here whether it's custom or built in")
+min(iterable, key="insert function here whether it's custom or built in")
