6394af357c365f2793e2e25bd707827d1caa92eb
chinna <bansuay88@gmail.com>
2025-12-04 12:19:52 +0700

    working on old work i missed out

---
+++ 02_Day_Variables_builtin_functions/useful built-in function.py
@@ -0,0 +1,9 @@
+enumerate(iterable, start=0) #useful in loop stuff
+abs(x) # return absolute value of that number
+all(iterable) # similar functional to and?
+any(iterable) # similar functioning to or?
+ascii(object) # return stri
+breakpoint(*arg, **kws) # not sure what it's purpose yet
+callable(object)
+chr(i) # return unicode point of an integer?
+delattr(object, name) # argument is object and string, get more information soon
